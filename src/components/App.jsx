08917c42667f2216f0682fe7cae5d3de57c3a878
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-03-28 16:31:21 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from '../redux/operations';
+import { getError, getIsLoading } from '../redux/selectors';
+
+import { Layout } from './Layout/Layout';
+import { AppBar } from './AppBar/AppBar';
+import { TaskForm } from './TaskForm/TaskForm';
+import { TaskList } from './TaskList/TaskList';
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <TaskList />
+    </Layout>
+  );
+};
